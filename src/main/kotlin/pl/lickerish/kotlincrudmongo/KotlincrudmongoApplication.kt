0fd311631049fe995d576a66ce6edd2dd5699c9a
lickerish <kacper.lakomski@gmail.com>
2019-10-28 17:30:31 +0100

    Init commit

---
+++ src/main/kotlin/pl/lickerish/kotlincrudmongo/KotlincrudmongoApplication.kt
@@ -0,0 +1,11 @@
+package pl.lickerish.kotlincrudmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlincrudmongoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlincrudmongoApplication>(*args)
+}
---
+++ src/test/kotlin/pl/lickerish/kotlincrudmongo/KotlincrudmongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.lickerish.kotlincrudmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlincrudmongoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
